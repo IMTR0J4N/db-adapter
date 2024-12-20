2c691a1c98b6f67b0a8a57f630049d715b50ca24
IMTR0J4N <jjorgebeau.pro@gmail.com>
2024-12-20 10:32:49 +0100

    Update bsonSerialize method return type in AbstractDocument class to use Document

--- src/Context/Class/AbstractDocument.php
+++ src/Context/Class/AbstractDocument.php
@@ -3,8 +3,10 @@
 namespace DBAdapter\Context\Class;
 
 use MongoDB\BSON\Persistable;
+use MongoDB\BSON\Document;
+use stdClass;
 
 abstract class AbstractDocument implements Persistable {
-    abstract public function bsonSerialize(): stdClass|MongoDB\BSON\Document|array;
+    abstract public function bsonSerialize(): stdClass|Document|array;
     abstract public function bsonUnserialize($data): void;
 }
